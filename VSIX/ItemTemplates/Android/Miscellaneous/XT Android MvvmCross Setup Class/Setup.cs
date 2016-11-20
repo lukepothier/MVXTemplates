25d84bd64bfe93b98a6db8568fdca98637356120
Luke Pothier <lukepothier@gmail.com>
2016-11-20 23:40:30 +0200

    Added some empty Android templates

---
+++ VSIX/ItemTemplates/Android/Miscellaneous/XT Android MvvmCross Setup Class/Setup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XT.VSIX.ItemTemplates.Android.Miscellaneous.XT_Android_MvvmCross_Setup_Class
+{
+    public class Setup
+    {
+    }
+}
