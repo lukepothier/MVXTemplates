ecd7dae0e459483219dc745ffb049f4ee43827b9
Plac3hold3r <Plac3hold3r+github@outlook.com>
2016-11-27 12:45:45 +0200

    iOS advanced setup fixed presenter creation

--- VSIX/ItemTemplates/iOS/Miscellaneous/XT iOS MvvmCross Setup Class - Advanced/Setup.cs
+++ VSIX/ItemTemplates/iOS/Miscellaneous/XT iOS MvvmCross Setup Class - Advanced/Setup.cs
@@ -7,8 +7,8 @@ namespace $rootnamespace$
 {
     public class Setup : MvxIosSetup
     {
-        public Setup(MvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter)
-            : base(applicationDelegate, presenter)
+        public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
+               : base(applicationDelegate, window)
         {
         }
 
@@ -39,21 +39,21 @@ namespace $rootnamespace$
 
         // A custom presenter is a necessity if an app employs complex navigation techniques like split-screens, tabs, and the like
         // Learn more about custom view presenters at https://slodge.blogspot.com/2013/06/presenter-roundup.html
-        protected override IMvxIosViewPresenter CreatePresenter() => new MyPresenter();
+        protected override IMvxIosViewPresenter CreatePresenter() => new MyCustomPresenter(ApplicationDelegate, Window);
 
         // TODO :: Benchmark and decide whether this is worth including
         // If the InitializeViewLookup override does not exist, MvvmCross will use reflection to map iOS views to ViewModels
         // Reflection is very expensive, and the cost can be avoided by providing a dictionary of your mappings here
         // At scale, apps will start up measurably faster if this override exists
         protected override void InitializeViewLookup()
-        {
-            var viewModelViewLookup = new Dictionary<Type, Type>()
-                {
-                    { typeof (MyViewModel), typeof(MyView) }
-                };
+            {
+                var viewModelViewLookup = new Dictionary<Type, Type>()
+                    {
+                        { typeof (MyViewModel), typeof(MyView) }
+                    };
 
-            var container = Mvx.Resolve<IMvxViewsContainer>();
-            container.AddAll(viewModelViewLookup);
+                var container = Mvx.Resolve<IMvxViewsContainer>();
+                container.AddAll(viewModelViewLookup);
+            }
         }
-    }
 }
