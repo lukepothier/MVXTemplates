c1f712e8755e4cf792db866fbcfdb6a1d097ec21
Luke Pothier <lukepothier@gmail.com>
2016-12-02 17:39:03 +0200

    Added iOS CollectionViewController

---
+++ VSIX/ItemTemplates/iOS/Views/MVX iOS FluentLayout CollectionViewController/CollectionCell.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Cirrious.FluentLayouts.Touch;
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$CollectionCell : MvxCollectionViewCell
+    {
+        public static readonly NSString Identifier = new NSString(nameof($safeitemname$CollectionCell));
+        MvxFluentBindingDescriptionSet<$safeitemname$CollectionCell, MyItemViewModel> _bindingSet;
+
+        public $safeitemname$CollectionCell(IntPtr handle)
+            : base(handle)
+        {
+            CreateViewElements();
+
+            LayoutViewElements();
+
+            Bind();
+        }
+
+        void CreateViewElements()
+        {
+            this.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+            AddSubviews(/* your views here */);
+        }
+
+        void LayoutViewElements()
+        {
+            this.AddConstraints(new FluentLayout[]
+            {
+
+            });
+        }
+
+        void Bind()
+        {
+            this.DelayBind(() =>
+            {
+                _bindingSet = this.CreateBindingSet<$safeitemname$CollectionCell, MyItemViewModel >();
+
+                _bindingSet.Apply();
+            });
+        }
+    }
+}
---
+++ VSIX/ItemTemplates/iOS/Views/MVX iOS FluentLayout CollectionViewController/CollectionSource.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using MvvmCross.Binding.iOS.Views;
+using UIKit;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$CollectionSource : MvxCollectionViewSource
+    {
+        public $safeitemname$CollectionSource(UICollectionView collectionView)
+            : base(collectionView)
+        {
+            collectionView.RegisterClassForCellReuse(typeof($safeitemname$CollectionCell), $safeitemname$CollectionCell.Identifier);
+        }
+
+        protected override UICollectionViewCell GetOrCreateCellFor(UICollectionView collectionView, NSIndexPath indexPath, object item)
+            => collectionView.DequeueReusableCell($safeitemname$CollectionCell.Identifier, indexPath);
+    }
+}
