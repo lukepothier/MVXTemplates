f6069d0f5025e1c166c4384d4f71239af93f95bb
Luke Pothier <lukepothier@gmail.com>
2016-12-04 21:02:23 +0200

    Added App.cs template

---
+++ VSIX/ItemTemplates/Shared/Miscellaneous/MVX MvvmCross App Class/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Platform.IoC;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<FirstViewModel>();
+        }
+    }
+}
