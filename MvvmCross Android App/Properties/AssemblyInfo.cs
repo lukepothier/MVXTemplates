9a750fbffe92eea9e3e51196acd0fa51847f4e89
Luke Pothier <lukepothier@gmail.com>
2016-12-29 17:56:33 +0200

    WIP Android App template - assembly references outstanding

--- MvvmCross Android App/Properties/AssemblyInfo.cs
+++ MvvmCross Android App/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvvmCross_Android_App")]
+[assembly: AssemblyTitle("$safeprojectname$")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvvmCross Android App")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("$safeprojectname$")]
+[assembly: AssemblyCopyright("Copyright ©  $year$")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
---
+++ MvvmCross Android App/Views/Activities/FirstActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace $safeprojectname$
+{
+    [Activity(Label = "FirstActivity", MainLauncher = true)]
+    public class FirstActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.layout_first);
+        }
+    }
+}
+
