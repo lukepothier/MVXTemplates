1a6c371391bfd097c3fc1ebecd8f80ef16799ce5
Luke Pothier <lukepothier@gmail.com>
2016-12-30 14:02:21 +0200

    Added iOS project

---
+++ MvvmCross iOS App/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using UIKit;
+
+namespace $safeprojectname$
+{
+    [Register(nameof(AppDelegate))]
+public class AppDelegate : UIApplicationDelegate
+{
+    public override UIWindow Window
+    {
+        get;
+        set;
+    }
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+        Window.MakeKeyAndVisible();
+
+        return true;
+    }
+}
+}
