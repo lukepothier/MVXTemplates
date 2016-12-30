33c03f8e2bf215a2f9e231e62e56e6053c4a2676
Luke Pothier <lukepothier@gmail.com>
2016-12-30 15:17:14 +0200

    Added MVX stuff to iOS app template

---
+++ MvvmCross iOS App/Views/FirstViewController.cs
@@ -0,0 +1,52 @@
+﻿using Cirrious.FluentLayouts.Touch;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using UIKit;
+
+namespace $safeprojectname$
+{
+    public class FirstViewController : MvxViewController<FirstViewModel>
+    {
+        MvxFluentBindingDescriptionSet<FirstViewController, FirstViewModel> _bindingSet;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            CreateViewElements();
+
+            LayoutViewElements();
+
+            Bind();
+        }
+
+        void CreateViewElements()
+        {
+            // Create your controls, and assign them to class-level variables the way that _bindingSet is assigned below.
+            // This pattern allows you to split up the process of creating and binding a UI into small, modular methods
+
+            // Calling this method allows FluentLayout to manage the layout of your UI
+            View.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+            // Don't forget to add your controls to the View
+            View.AddSubviews(/* your views here */);
+        }
+
+        void LayoutViewElements()
+        {
+            View.AddConstraints(new FluentLayout[]
+            {
+                // For FluentLayout samples and how-tos, see https://github.com/FluentLayout/Cirrious.FluentLayout#fluentlayout
+            });
+        }
+
+        void Bind()
+        {
+            _bindingSet = this.CreateBindingSet<FirstViewController, FirstViewModel>();
+
+            // Bind your UI elements to ViewModel members using _bindingSet.Bind
+
+            _bindingSet.Apply();
+        }
+    }
+}
