10d0f33ec38de5b8bd3d9f2ac09ae303554b3820
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-01-03 00:09:16 +0200

    Added item templates for ValueConverters, including Color and Visibility

---
+++ VSIX/ItemTemplates/Shared/Miscellaneous/MVX MvvmCross ColorConverter/ColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Platform.UI;
+using MvvmCross.Plugins.Color;
+
+namespace $rootnamespace$
+{
+    // Requires MvvmCross Color Plugin install:
+    // https://www.nuget.org/packages/MvvmCross.Plugin.Color/
+    // For more information on how to use Value Converter see:
+    // https://mvvmcross.com/docs/value-converters
+    public class $safeitemname$ : MvxColorValueConverter<ViewModelPropertyType>
+    {
+        // Used for changing ViewModel values into View colors
+        protected override MvxColor Convert(ViewModelPropertyType value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VSIX/ItemTemplates/Shared/Miscellaneous/MVX MvvmCross ValueConverter/ValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using MvvmCross.Platform.Converters;
+
+namespace $rootnamespace$
+{
+    // For more information on how to use Value Converter see:
+    // https://mvvmcross.com/docs/value-converters
+    public class $safeitemname$ : MvxValueConverter<ViewModelPropertyType, ViewPropertyType>
+    {
+        // Used for changing ViewModel values into View values
+        protected override ViewPropertyType Convert(ViewModelPropertyType value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    
+        // Used for changing View values into ViewModel values
+        // Can be removed if only one-way binding (ViewModel -> View) is being used
+        protected override ViewModelPropertyType ConvertBack(ViewPropertyType value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
