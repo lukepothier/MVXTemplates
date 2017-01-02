96f00140fa649a10529621ad0d3d44ef7d0b71a6
Luke Pothier <lukepothier@gmail.com>
2017-01-03 00:39:43 +0200

    Added MVX JSON plugin to iOS app template

---
+++ MvvmCross iOS App/Bootstrap/JsonPluginBootstrap.cs
@@ -0,0 +1,10 @@
+using MvvmCross.Platform.Plugins;
+using MvvmCross.Plugins.Json;
+
+namespace $safeprojectname$.Bootstrap
+{
+    public class JsonPluginBootstrap
+        : MvxPluginBootstrapAction<PluginLoader>
+    {
+    }
+}
