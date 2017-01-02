9cf48ef08ba1c407352e6fb579a22a8a859ba0b6
Luke Pothier <lukepothier@gmail.com>
2017-01-03 00:40:33 +0200

    Added namespace to JsonPluginBootstrap in Android app template

--- MvvmCross Android App/Bootstrap/JsonPluginBootstrap.cs
+++ MvvmCross Android App/Bootstrap/JsonPluginBootstrap.cs
@@ -1,9 +1,10 @@
 using MvvmCross.Platform.Plugins;
+using MvvmCross.Plugins.Json;
 
-namespace .Bootstrap
+namespace $safeprojectname$.Bootstrap
 {
     public class JsonPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
+        : MvxPluginBootstrapAction<PluginLoader>
     {
     }
 }
