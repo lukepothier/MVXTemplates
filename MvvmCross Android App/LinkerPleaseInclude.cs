0785e016261e38e9c7b7e093a534a4052fa623cb
Luke Pothier <lukepothier@gmail.com>
2017-01-03 20:52:46 +0200

    Updated Android app LinkerPleaseInclude.cs

--- MvvmCross Android App/LinkerPleaseInclude.cs
+++ MvvmCross Android App/LinkerPleaseInclude.cs
@@ -1,6 +1,47 @@
+using System.Collections.Specialized;
+using System.Windows.Input;
+using Android.Widget;
+
 namespace $safeprojectname$
 {
+	// LinkerPleaseInclude is never actually executed, but when Xamarin linking is enabled it ensures types and properties are preserved in the deployed app
+    // Included are some examples of types and properties you may need preserved in a linked version of your app
     public class LinkerPleaseInclude
     {
+        public void Include(Button button)
+        {
+            button.Click += (s, e) => button.Text = $"{button.Text}";
+        }
+
+        public void Include(CheckBox checkBox)
+        {
+            checkBox.CheckedChange += (sender, args) => checkBox.Checked = !checkBox.Checked;
+        }
+
+        public void Include(TextView text)
+        {
+            text.TextChanged += (sender, args) => text.Text = $"{text.Text}";
+            text.Hint = $"{text.Hint}";
+        }
+
+        public void Include(CompoundButton cb)
+        {
+            cb.CheckedChange += (sender, args) => cb.Checked = !cb.Checked;
+        }
+
+        public void Include(SeekBar sb)
+        {
+            sb.ProgressChanged += (sender, args) => sb.Progress = sb.Progress + 1;
+        }
+
+        public void Include(INotifyCollectionChanged changed)
+        {
+            changed.CollectionChanged += (s, e) => { var test = $"{e.Action}{e.NewItems}{e.NewStartingIndex}{e.OldItems}{e.OldStartingIndex}"; };
+        }
+
+        public void Include(ICommand command)
+        {
+            command.CanExecuteChanged += (s, e) => { if (command.CanExecute(null)) command.Execute(null); };
+        }
     }
 }
