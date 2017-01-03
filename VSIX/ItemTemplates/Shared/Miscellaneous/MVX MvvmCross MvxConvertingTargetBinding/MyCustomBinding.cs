7ea88569be198f01bddc47e23f325654e3895a49
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-01-03 15:46:12 +0200

    Added template for custom binding using MvxConvertingTargetBinding

---
+++ VSIX/ItemTemplates/Shared/Miscellaneous/MVX MvvmCross MvxConvertingTargetBinding/MyCustomBinding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Bindings.Target;
+
+namespace $rootnamespace$
+{
+    // For more information on custom binding see:
+    // https://mvvmcross.com/docs/customize-app#section-registering-custom-bindings
+    // http://slodge.blogspot.co.za/2013/06/n28-custom-bindings-n1-days-of-mvvmcross.html
+    public class $safeitemname$ : MvxConvertingTargetBinding
+    {
+        public $safeitemname$(ControlType target) : base(target)
+        {
+        }
+    
+        public override Type TargetType => typeof(ControlType);
+
+        // For more information on the different binding mode options see:
+        // https://mvvmcross.com/docs/data-binding#section-binding-modes
+        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWay;
+    
+        protected override void SetValueImpl(object target, object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
