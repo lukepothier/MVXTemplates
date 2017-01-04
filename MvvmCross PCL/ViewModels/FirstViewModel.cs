b7a6bc192477548adf2b642835efd0fa2b6b6085
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-01-04 23:29:11 +0200

    Fixed first VM formatting

--- MvvmCross PCL/ViewModels/FirstViewModel.cs
+++ MvvmCross PCL/ViewModels/FirstViewModel.cs
@@ -3,21 +3,21 @@
 namespace $safeprojectname$.ViewModels
 {
     public class FirstViewModel : MvxViewModel
-{
-
-    #region Commands
-
-    IMvxCommand _goForwardCommand;
-    public IMvxCommand GoForwardCommand =>
-        _goForwardCommand ?? (_goForwardCommand = new MvxCommand(GoForward));
-
-    #endregion
-
-    #region Helpers
-
-    void GoForward() => ShowViewModel<SecondViewModel>();
-
-    #endregion
-
-}
+    {
+    
+        #region Commands
+    
+        IMvxCommand _goForwardCommand;
+        public IMvxCommand GoForwardCommand =>
+            _goForwardCommand ?? (_goForwardCommand = new MvxCommand(GoForward));
+    
+        #endregion
+    
+        #region Helpers
+    
+        void GoForward() => ShowViewModel<SecondViewModel>();
+    
+        #endregion
+    
+    }
 }
