d3dfe11d1728e04a8edfd701cdba626066adf730
Luke Pothier <lukepothier@gmail.com>
2017-01-19 19:40:42 +0200

    Finalised Core item templates

--- VSIX/ItemTemplates/Xamarin/ViewModel/ViewModel.cs
+++ VSIX/ItemTemplates/Xamarin/ViewModel/ViewModel.cs
@@ -8,6 +8,8 @@ namespace $rootnamespace$
         {
         }
 
+        // Init and Start are important parts of MvvmCross' CIRS ViewModel lifecycle
+        // Learn how to use Init and Start at https://github.com/MvvmCross/MvvmCross/wiki/view-model-lifecycle
         public void Init()
         {
         }
